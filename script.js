caa140db1cd1832f6a2bd71d3152e136329c77b6
Muhammad Indra <m.indra.vm@gmail.com>
2025-10-03 15:33:08 +0700

    Add getComputerChoice function
    This function is used to get computer random choice based on random  integer

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+// function to get random number
+function getRandomInt(maxNum){
+  return Math.floor(Math.random() * maxNum);
+}
+
+// function to get computer choice
+function getComputerChoice(){
+  let result;
+  let randomNum = getRandomInt(3);
+
+  if (randomNum == 0){
+    result = "rock";
+  } else if(randomNum == 1){
+    result = "paper";
+  } else {
+    result = "scissors"
+  }
+
+  return result;
+}
 
