6b56ae1efff45c9e880a6d869dff93c5ca4e718c
Muhammad Indra <m.indra.vm@gmail.com>
2025-10-03 15:45:44 +0700

    Add getHumanChoice function
    This function is used to get human choice using prompt function

--- script.js
+++ script.js
@@ -10,12 +10,17 @@ function getComputerChoice(){
 
   if (randomNum == 0){
     result = "rock";
-  } else if(randomNum == 1){
+  } else if (randomNum == 1){
     result = "paper";
   } else {
-    result = "scissors"
+    result = "scissors";
   }
 
   return result;
+}
+
+// function to get human choice
+function getHumanChoice(){
+  return prompt("Please type one of the choices : rock, paper, scissors");
 }
 
